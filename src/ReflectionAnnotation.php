1d75308f84342e7f39d2221f3e955faab4fb1425
clover <370546391@qq.com>
2017-07-30 03:13:18 +0800

    修复不能解析json的问题

--- src/ReflectionAnnotation.php
+++ src/ReflectionAnnotation.php
@@ -9,14 +9,16 @@ namespace Joking\Annotation;
 abstract class ReflectionAnnotation {
 
     protected function match($document) {
-        if (preg_match('#^/\*\*(.*)\*/#s', $document, $comment) === false || empty($comment)) {
+        $position = $this->findInitialTokenPosition($document);
+        if ($position === null) {
             return [];
         }
 
-        $comment = trim($comment[1]);
-        if (preg_match_all('#^\s*\*\s*\@(.[A-Za-z]{1,})\((.*)\)#m', $comment, $lines) === false) {
+        $comment = trim(substr($document, $position), '* /');
+        if (preg_match_all('#\@(.[A-Za-z]{0,})\((.*)\)#m', $comment, $lines) === false) {
             return [];
         }
+
 
         $target = [];
         for ($i = 0; $i < count($lines[1]); $i++) {
@@ -26,25 +28,69 @@ abstract class ReflectionAnnotation {
         return $this->parseParams($target);
     }
 
+    function findInitialTokenPosition($input) {
+        $pos = 0;
+        while (($pos = strpos($input, '@', $pos)) !== false) {
+            if ($pos === 0 || $input[$pos - 1] === ' ' || $input[$pos - 1] === '*') {
+                return $pos;
+            }
+
+            $pos++;
+        }
+
+        return null;
+    }
+
     /**
      * @param $target
      * @return array
      */
     protected function parseParams($target) {
+        $r = array(
+            '[a-z_\\\][a-z0-9_\:\\\]*[a-z_][a-z0-9_]*',
+            '(?:[+-]?[0-9]+(?:[\.][0-9]+)*)(?:[eE][+-]?[0-9]+)?',
+            '"(?:""|[^"])*+"',
+        );
+        $non = array('\s+', '\*+', '(.)');
+
+        $regex = sprintf('/(%s)|%s/%s', implode(')|(', $r), implode('|', $non), 'i');
+        $flags = PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_OFFSET_CAPTURE;
+
         $result = [];
         foreach ($target as $name => $value) {
-            if (strpos($value, ',')) {
-                $num = 1;
-                foreach (explode(',', $value) as $param) {
-                    list($k, $v) = $this->parseParam($param, $num === 1);
-                    $result[$name][$k] = $v;
-                    $num++;
-                }
-            } else if (empty($value)) {
+            if (empty($value)) {
                 $result[$name] = [];
-            } else {
-                list($k, $v) = $this->parseParam($value, true);
+                continue;
+            }
+
+            $matches = preg_split($regex, $value, -1, $flags);
+
+            $array = [];
+            $string = '';
+            $openh = true;
+            $opens = true;
+            foreach ($matches as $match) {
+
+                $match[0] == '{' && $openh = false;
+                $match[0] == '}' && $openh = true;
+                $match[0] == '[' && $opens = false;
+                $match[0] == ']' && $opens = true;
+
+                if ($match[0] == "," && $openh == true && $opens == true) {
+                    $array[] = $string;
+                    $string = '';
+                    continue;
+                }
+
+                $string .= $match[0];
+            }
+
+            empty($string) || $array[] = $string;
+            $num = 1;
+            for ($i = 0; $i < count($array); $i++) {
+                list($k, $v) = $this->parseParam($array[$i], $num === 1);
                 $result[$name][$k] = $v;
+                $num++;
             }
         }
 
@@ -62,18 +108,28 @@ abstract class ReflectionAnnotation {
         if (strpos($param, '=')) {
             $array = explode('=', $param);
             for ($i = 0; $i < count($array); $i++) {
-                $array[$i] = trim($array[$i], " \t\n\r\0\x0B\"\'");
+                $array[$i] = $this->disposeValue($array[$i]);
             }
 
             return $array;
         }
 
         if ($hasDefault) {
-            return ['firstDefaultParam', trim($param, " \t\n\r\0\x0B\"\'")];
+            return ['firstDefaultParam', $this->disposeValue($param)];
         }
 
-        throw new \Exception('无法格式化参数');
+        throw new \Exception('无法格式化参数：' . $param);
     }
+
+    protected function disposeValue($value) {
+        $value = trim($value, " \t\n\r\0\x0B\"\'");
+        if ($jsonValue = json_decode($value)) {         //字符串不是json不操作,是json则转成数组
+            return $jsonValue;
+        } else {
+            return $value;
+        }
+    }
+
 
     /**
      * 创建标签实体类
